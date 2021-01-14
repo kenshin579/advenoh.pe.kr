4d7628180b0925dedd437cbdc6246515fe7de3fc
kenshin579 <kenshin579@hotmail.com>
2021-01-14 11:00:20 +0900

    [ADV-1] content 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Advenoh - Gatsby Starter Portfolio`,
   },
   plugins: [
     {
@@ -26,9 +26,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Advenoh - @lekoarts/gatsby-theme-cara`,
+        short_name: `Advenoh`,
+        description: `Advenoh My Projects`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
